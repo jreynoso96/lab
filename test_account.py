63a02cca68a7ea63190faa9c9d16ebaf53bcac0c
jreynoso96 <78325030+jreynoso96@users.noreply.github.com>
2023-04-21 22:18:49 -0400

    Update test_account.py

--- test_account.py
+++ test_account.py
@@ -1,1 +1,34 @@
+import unittest
 from account import *
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.a1 = Account('John')
+        self.a2 = Account('Daisy')
+
+    def tearDown(self):
+        del self.a1
+        del self.a2
+
+    def test_init(self):
+        self.assertEqual(self.a1.get_name(),'John')
+        self.assertEqual(self.a2.get_name(),'Daisy')
+        self.assertEqual(self.a1.get_balance(),0)
+        self.assertEqual(self.a2.get_balance(),0)
+
+        self.assertTrue(self.a1.deposit(50))
+        self.assertFalse(self.a2.deposit(-50))
+
+        self.assertEqual(self.a1.get_balance(),50)
+        self.assertEqual(self.a2.get_balance(), 0)
+
+        self.assertTrue(self.a1.withdraw(25))
+        self.assertFalse(self.a2.withdraw(50))
+        self.assertFalse(self.a2.withdraw(-50))
+
+        self.assertEqual(self.a1.get_balance(), 25)
+        self.assertEqual(self.a2.get_balance(), 0)
+
+if __name__ == "__main__":
+    unittest.main()
