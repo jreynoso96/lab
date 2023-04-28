fcc9ee87ae99bace1249318317fb096470bc857e
jreynoso96 <78325030+jreynoso96@users.noreply.github.com>
2023-04-28 19:36:31 -0400

    Update test_account.py

--- test_account.py
+++ test_account.py
@@ -1,5 +1,5 @@
 import pytest
-from Lab12 import *
+from account import *
 
 class Test:
     def setup_method(self):
@@ -17,25 +17,43 @@ class Test:
         assert self.a2.get_balance() == 0
 
     def test_deposit(self):
-        assert self.a1.deposit(50.0) == True
+        assert self.a1.deposit(0.0) == False
+        assert self.a1.get_balance() == 0
         assert self.a1.deposit(-50.0) == False
+        assert self.a1.get_balance() == 0
+        assert self.a1.deposit(50.0) == True
         assert self.a1.get_balance() == 50.0
 
-        assert self.a2.deposit(100.0) == True
+        assert self.a2.deposit(0.0) == False
+        assert self.a2.get_balance() == 0
         assert self.a2.deposit(-5.0) == False
+        assert self.a2.get_balance() == 0
+        assert self.a2.deposit(100.0) == True
         assert self.a2.get_balance() == 100.0
 
     def test_withdraw(self):
-        assert self.withdraw(5.0) == False
-        assert self.a1.deposit(50.0) == True
-        assert self.a1.withdraw(100.0) == False
         assert self.a1.withdraw(-50.0) == False
+        assert self.a1.get_balance() == 0
+        assert self.a1.withdraw(0.0) == False
+        assert self.a1.get_balance() == 0
+        assert self.a1.deposit(50.0) == True
+        assert self.a1.get_balance() == 50.0
+        assert self.a1.withdraw(100.0) == False
+        assert self.a1.get_balance() == 50.0
+        assert self.a1.withdraw(-50.0) == False
+        assert self.a1.get_balance() == 50.0
         assert self.a1.withdraw(20.0) == True
         assert self.a1.get_balance() == 30.0
 
-        assert self.withdraw(-50.0) == False
+        assert self.a2.withdraw(-50.0) == False
+        assert self.a2.get_balance() == 0.0
+        assert self.a2.withdraw(0.0) == False
+        assert self.a2.get_balance() == 0.0
         assert self.a2.deposit(100.0) == True
+        assert self.a2.get_balance() == 100.0
         assert self.a2.withdraw(-100.0) == False
+        assert self.a2.get_balance() == 100.0
         assert self.a2.withdraw(2000.0) == False
+        assert self.a2.get_balance() == 100.0
         assert self.a2.withdraw(50.0) == True
         assert self.a2.get_balance() == 50.0
