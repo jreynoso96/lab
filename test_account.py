fbc663d8d934215f3a943c379c5062119c1e772f
jreynoso96 <78325030+jreynoso96@users.noreply.github.com>
2023-04-28 19:29:49 -0400

    Update test_account.py

--- test_account.py
+++ test_account.py
@@ -1,34 +1,41 @@
-import unittest
-from account import *
+import pytest
+from Lab12 import *
 
-
-class Test(unittest.TestCase):
-    def setUp(self):
+class Test:
+    def setup_method(self):
         self.a1 = Account('John')
         self.a2 = Account('Daisy')
 
-    def tearDown(self):
+    def teardown_method(self):
         del self.a1
         del self.a2
 
     def test_init(self):
-        self.assertEqual(self.a1.get_name(),'John')
-        self.assertEqual(self.a2.get_name(),'Daisy')
-        self.assertEqual(self.a1.get_balance(),0)
-        self.assertEqual(self.a2.get_balance(),0)
+        assert self.a1.get_name() == 'John'
+        assert self.a2.get_name() == 'Daisy'
+        assert self.a1.get_balance() == 0
+        assert self.a2.get_balance() == 0
 
-        self.assertTrue(self.a1.deposit(50))
-        self.assertFalse(self.a2.deposit(-50))
+    def test_deposit(self):
+        assert self.a1.deposit(50.0) == True
+        assert self.a1.deposit(-50.0) == False
+        assert self.a1.get_balance() == 50.0
 
-        self.assertEqual(self.a1.get_balance(),50)
-        self.assertEqual(self.a2.get_balance(), 0)
+        assert self.a2.deposit(100.0) == True
+        assert self.a2.deposit(-5.0) == False
+        assert self.a2.get_balance() == 100.0
 
-        self.assertTrue(self.a1.withdraw(25))
-        self.assertFalse(self.a2.withdraw(50))
-        self.assertFalse(self.a2.withdraw(-50))
+    def test_withdraw(self):
+        assert self.withdraw(5.0) == False
+        assert self.a1.deposit(50.0) == True
+        assert self.a1.withdraw(100.0) == False
+        assert self.a1.withdraw(-50.0) == False
+        assert self.a1.withdraw(20.0) == True
+        assert self.a1.get_balance() == 30.0
 
-        self.assertEqual(self.a1.get_balance(), 25)
-        self.assertEqual(self.a2.get_balance(), 0)
-
-if __name__ == "__main__":
-    unittest.main()
+        assert self.withdraw(-50.0) == False
+        assert self.a2.deposit(100.0) == True
+        assert self.a2.withdraw(-100.0) == False
+        assert self.a2.withdraw(2000.0) == False
+        assert self.a2.withdraw(50.0) == True
+        assert self.a2.get_balance() == 50.0
